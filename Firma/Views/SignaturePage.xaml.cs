1ad5f84e8f8fa00c2b34c2bb067f66327a2a82c0
Dans-sys <danielsolano712@gmail.com>
2021-10-31 05:22:58 -0600

    pantalla de firma

---
+++ Firma/Views/SignaturePage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Firma.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SignaturePage : ContentPage
+    {
+        public SignaturePage()
+        {
+            InitializeComponent();
+        }
+
+        private async Task<bool> ValidationSignature()
+        {
+            if (String.IsNullOrWhiteSpace(Nombre.Text))
+            {
+                await this.DisplayAlert("Advertencia", "Debe describir un nombre", "OK");
+                return false;
+            }
+
+            if (String.IsNullOrWhiteSpace(Descripcion.Text))
+            {
+                await this.DisplayAlert("Advertencia", "Debe escribir una descripción", "OK");
+                return false;
+            }
+
+            if (Signature.IsBlank)
+            {
+                await this.DisplayAlert("Advertencia", "Escriba su firma", "OK");
+                return false;
+            }
+
+            return true;
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            //Boton de Guardar
+            if (await ValidationSignature())
+            {
+                await DisplayAlert("Exito", "Guardado Correctamente", "OK");
+                await Navigation.PushAsync(new SignaturePage());
+            }
+        }
+    }
+}
